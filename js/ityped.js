d87150d571079233cb93e516f43fc60a16577708
RyosukeFujita <fujitatsumiki@gmail.com>
2023-03-05 20:28:34 +0900

    Up

---
+++ js/ityped.js
@@ -0,0 +1,7 @@
+ityped.init('#ityped', {
+  strings:['Hello World. . . . .', 'This is the portfolio place.', "I will post my growth here.", 
+  "英語でカッコつけました.","今まで作成したコンテンツ.","ドキュメント等を.","ここに残していきます.",". . . 壁|ω･`)ﾁﾗｯ ",'Hello World. . . . .', 'This is the portfolio place.', "I will post my growth here.", 
+  "英語でカッコつけました.","今まで作成したコンテンツ.","ドキュメント等を.","ここに残していきます.",". . . 壁|ω･`)ﾁﾗｯ ","もう流石に見てないよね？","(´･ω･`;A) .",". . ._(⌒(_´-ω-`)_. . ." ],
+  startDelay: 200,
+  loop: true
+});
