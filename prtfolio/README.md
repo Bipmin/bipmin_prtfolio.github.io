f61f08240eea960600e2419e12395f3de577b766
RyosukeFujita <fujitatsumiki@gmail.com>
2023-03-05 10:43:16 +0900

    Up

---
+++ prtfolio/README.md
@@ -0,0 +1,1 @@
+# https-bipmin_oprtfolio.github.io
---
+++ prtfolio/_script/modaalin.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+	$(".inline").modaal({
+		content_source: '#inline',
+		animation: 'slide-down',
+		animation_speed: '500'
+	});
+});
+
+$(document).ready(function () {
+	$(".inline_Movie").modaal({
+		content_source: '#inline_Movie',
+		animation: 'slide-down',
+		animation_speed: '500'
+	});
+});
